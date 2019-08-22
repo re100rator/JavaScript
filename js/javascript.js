53ddd53b2e9c7ad41b3cc311445c616a85382604
Aleksey <1703orsk@gmail.com>
2019-08-22 19:48:29 +0500

    Выполнил задание

--- js/javascript.js
+++ js/javascript.js
@@ -1,2 +1,23 @@
-let a = alert("есть ли тебе 18?");
-console.log(typeof(a));
+let money = prompt("Ваш бюджет на месяц");
+
+let time = prompt("Введите дату в формате YYYY-MM-DD");
+
+let a = prompt("Введите статью на обязательные расходы в месяц");
+let b = prompt("Во сколько обойдется?");
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {
+        c: a + " " + b
+    },
+    optionalExpenses: {
+
+    },
+    income: [],
+    savings: false
+};
+
+alert("Бюджет на один день " + money/30); 
+
+console.log(appData.expenses.c);
